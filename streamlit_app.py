b19b9ae87b06bc28fda965e4fac798559b6f7bf1
Lyle <107318190+Vaendrin@users.noreply.github.com>
2023-04-30 12:40:10 +0200

    Variable File detection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 from math import floor
 from typing import List
 
@@ -30,7 +31,7 @@ def fetch(session, url):
 
 # hashed_passwords = stauth.Hasher(['abc123','def']).generate()
 
-with open(r'C:\Users\User\Desktop\4.Semester\Web_Programming\TraderJoe\config.yaml') as file:
+with open(os.getcwd() + "\config.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
     authenticator = Authenticate(
@@ -46,7 +47,7 @@ with open(r'C:\Users\User\Desktop\4.Semester\Web_Programming\TraderJoe\config.ya
 session = requests.Session()
 
 stocks_list = fetch(session, f"http://127.0.0.1:8084/investment/assets")
-#print(stocks_list)
+# print(stocks_list)
 # dictionary for the categories
 stocks_category = stocks_list
 if authentication_status:
@@ -66,7 +67,7 @@ if authentication_status:
                 option = option.replace("/", "-")
             with st.container():
                 trading_options = ["Minute Trading", "Hour Trading", "Day Trading"]
-                trading_option = st.selectbox(label="Select Trading Type",options=trading_options)
+                trading_option = st.selectbox(label="Select Trading Type", options=trading_options)
             with st.container():
                 col1, col2 = st.columns(2)
                 with col1:
@@ -76,14 +77,13 @@ if authentication_status:
                 with col2:
                     end_date = st.date_input(
                         "At which date should the chart end?",
-                        datetime.date(2023,4,28), max_value=datetime.date.today())
+                        datetime.date(2023, 4, 28), max_value=datetime.date.today())
             with st.container():
                 col1, col2 = st.columns(2)
                 with col1:
                     start_time = st.time_input("At which time should the chart start?", datetime.time(00, 00, 00))
                 with col2:
-                    end_time = st.time_input("at which time should the chart end?", datetime.time(15,00,00))
-
+                    end_time = st.time_input("at which time should the chart end?", datetime.time(15, 00, 00))
 
         if st.sidebar.button("Submit"):
             temp_start_date_time = datetime.datetime.combine(start_date, start_time)
@@ -94,7 +94,8 @@ if authentication_status:
             print(start_date_time)
             print(end_date_time)
 
-            sample = fetch(session, f"http://127.0.0.1:8084/investment/{stocks_category[option]}/{start_date_time}/{end_date_time}/{trading_option}")
+            sample = fetch(session,
+                           f"http://127.0.0.1:8084/investment/{stocks_category[option]}/{start_date_time}/{end_date_time}/{trading_option}")
             try:
                 sample_data = pd.read_json(sample, orient="columns")
                 state.sample_data = sample_data
@@ -102,17 +103,15 @@ if authentication_status:
             except:
                 st.error("Sorry, no data available. We are working on it")
 
-             # update the session state with new sample_data
-
+            # update the session state with new sample_data
 
         # use the session state to access sample_data
 
-
-
         # Navigation Bar
         with st.sidebar:
-            choose = option_menu("Trading Strategy", ["Support and Resistance", "Momentum", "Bollinger", "Paper Trading","Account Details"])
-
+            choose = option_menu("Trading Strategy",
+                                 ["Support and Resistance", "Momentum", "Bollinger", "Paper Trading",
+                                  "Account Details"])
 
         if choose == "Support and Resistance":
             try:
@@ -124,18 +123,18 @@ if authentication_status:
         elif choose == "Momentum":
             st.title('Momentum')
             try:
-            #Calling the momentum method
+                # Calling the momentum method
                 sample_data = apply_momentum_strategy(sample_data)
                 st.write(sample_data)
 
-            # Create chart data from sample data
+                # Create chart data from sample data
 
                 chart_data = st.line_chart(sample_data['Cumulative Returns'])
 
-                    # Add buy and sell signals to chart data
+                # Add buy and sell signals to chart data
                 chart_data = add_signals_to_chart(chart_data, sample_data)
             except Exception:
-                    st.warning("Please submit the form to see the results")
+                st.warning("Please submit the form to see the results")
 
         elif choose == "Paper Trading":
             st.write('Paper Trading')
@@ -169,36 +168,36 @@ if authentication_status:
             tab1, tab2, tab3 = st.tabs(["Account Details", "Open Orders", "Closed Orders"])
             with tab1:
                 st.subheader("Account Details")
-                sample:dict = fetch(session,
-                               f"http://127.0.0.1:8084/investment/account")
+                sample: dict = fetch(session,
+                                     f"http://127.0.0.1:8084/investment/account")
                 sample = sample.get('_raw')
                 col1, col2 = st.columns(2)
                 with col1:
                     st.write('Account Number: ')
                 with col2:
                     st.write(sample.get('account_number'))
-                col1,col2 = st.columns(2)
+                col1, col2 = st.columns(2)
                 with col1:
                     st.write('Cash: ')
                 with col2:
                     st.write(sample.get('cash'))
-                col1,col2 = st.columns(2)
+                col1, col2 = st.columns(2)
                 with col1:
                     st.write('Daytrade count: ')
                 with col2:
-                    day_trade_count:int = sample.get('daytrade_count')
+                    day_trade_count: int = sample.get('daytrade_count')
                     st.write(str(day_trade_count))
-                col1,col2 = st.columns(2)
+                col1, col2 = st.columns(2)
                 with col1:
                     st.write('Status: ')
                 with col2:
                     st.write(sample.get('status'))
-                col1,col2 = st.columns(2)
+                col1, col2 = st.columns(2)
                 with col1:
                     st.write("Portfolio development: ")
 
                 with col2:
-                    new_equity:float = float(sample.get('equity'))-float(sample.get('last_equity'))
+                    new_equity: float = float(sample.get('equity')) - float(sample.get('last_equity'))
                     st.write(str(new_equity))
             with tab2:
                 st.subheader("Open Orders")
@@ -216,21 +215,19 @@ if authentication_status:
                 if sample == []:
                     st.warning("No open orders")
                 for row in sample:
-                    open_order:dict = row.get('_raw')
+                    open_order: dict = row.get('_raw')
 
-
-                    col1,col2, col3 = st.columns(3)
+                    col1, col2, col3 = st.columns(3)
                     with col1:
                         st.write(open_order.get('symbol'))
                     with col2:
                         st.write(open_order.get('qty'))
                     with col3:
-                        date:str = open_order.get('submitted_at')
+                        date: str = open_order.get('submitted_at')
                         date = date.replace("T", " ")
                         date = date.removesuffix("Z")
                         date = date.split(".")[0]
                         st.write(date)
-
 
             with tab3:
                 st.subheader("Closed Orders")
@@ -247,7 +244,7 @@ if authentication_status:
                     st.warning("No closed orders")
                 else:
                     for row in sample:
-                        open_order:dict = row.get('_raw')
+                        open_order: dict = row.get('_raw')
 
                     col1, col2 = st.columns(2)
                     with col1:
@@ -258,7 +255,6 @@ if authentication_status:
                         date = date.removesuffix("Z")
                         date = date.split(".")[0]
                         st.write(date)
-
 
 
     # ------METHODS--------------------
